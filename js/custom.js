123b28c503b7ca678bd3700b88ebd7e11b1fc908
Lazar Milosavljević <lazar.milosavljevic.2318@metropolitan.ac.rs>
2016-03-30 01:08:04 +0200

    Added functionality: get data about current weather in San Francisco from Wunderground API using jQuery and display data on contact page.

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,15 @@ $("small:first")
     .mouseleave(function() {
         $(this).css('color', '#777');
     });
+
+$(function()
+{
+    if($("#city").length)
+    {
+        $.getJSON("http://api.wunderground.com/api/aee8c654faac8b92/conditions/q/CA/San_Francisco.json", function(data){
+            $("#temperaturaC").empty().append("Temperatura [C]: " + JSON.stringify(data["current_observation"].temp_c));
+            $("#temperaturaF").empty().append("Temperatura [F]: " + JSON.stringify(data["current_observation"].temp_f));
+            $("<center><img src = " + JSON.stringify(data["current_observation"].icon_url) + " /></center>").insertAfter($("#city"));
+        });
+    }
+});
